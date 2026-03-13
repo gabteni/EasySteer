e58558a26de89040f56f726c25e46172c7052536
gabteni <48386716+gabteni@users.noreply.github.com>
2026-03-14 00:24:29 +0100

    Add SteerMOEExtractor and MoESteeringWeights

--- easysteer/steer/__init__.py
+++ easysteer/steer/__init__.py
@@ -9,39 +9,44 @@ from .pca import PCAExtractor
 from .lat import LATExtractor
 from .linear_probe import LinearProbeExtractor
 from .sae import SAEFeatureExplorer, search_sae_features, get_sae_feature_explanation, extract_sae_decoder_vector
+from .steer_moe import SteerMOEExtractor, MoESteeringWeights
 from .unified_interface import (
     extract_statistical_control_vector,
     extract_diffmean_control_vector,
     extract_pca_control_vector,
     extract_lat_control_vector,
-    extract_linear_probe_control_vector
+    extract_linear_probe_control_vector,
+    extract_steer_moe_weights,
 )
 
 __all__ = [
     # Core classes
     'StatisticalControlVector',
-    
+    'MoESteeringWeights',
+
     # Utility functions
     'extract_token_hiddens',
     'extract_last_token_hiddens',
     'extract_all_token_hiddens',
-    
+
     # Extractor classes
     'DiffMeanExtractor',
     'PCAExtractor',
     'LATExtractor',
     'LinearProbeExtractor',
     'SAEFeatureExplorer',
-    
+    'SteerMOEExtractor',
+
     # Unified interface functions
     'extract_statistical_control_vector',
     'extract_diffmean_control_vector',
     'extract_pca_control_vector',
     'extract_lat_control_vector',
     'extract_linear_probe_control_vector',
-    
+    'extract_steer_moe_weights',
+
     # SAE feature exploration functions
     'search_sae_features',
     'get_sae_feature_explanation',
-    'extract_sae_decoder_vector'
-] 
+    'extract_sae_decoder_vector',
+]
